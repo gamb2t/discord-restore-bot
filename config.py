7d3694f303e22510502c2e85dad5643da3d958c4
gamb1t <74240139+gamb2t@users.noreply.github.com>
2021-11-16 18:17:32 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 token = "봇 토큰"
 oauth2_url = "Oauth2 인증 URL"
 recover_key = "복구 시 사용할 복구 키"
+guildid = "서버아이디"
+roleid = "인증시 부여할 역할 아이디"
 
 API_ENDPOINT = "https://discord.com/api/v9" # 건들지 말것
 CLIENT_ID = "Oauth2 클라이언트 아이디"
